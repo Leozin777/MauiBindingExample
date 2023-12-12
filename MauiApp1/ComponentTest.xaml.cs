31bbbbbc60b6e6f5bf382e2a08c9400e1a2276c6
Daniel Ancines <daniel.ancines@gmail.com>
2023-12-12 08:42:51 -0300

    Making dependency property working

--- MauiApp1/ComponentTest.xaml.cs
+++ MauiApp1/ComponentTest.xaml.cs
@@ -1,19 +1,35 @@
+
+
 namespace MauiApp1;
 
 public partial class ComponentTest : ContentView
 {
-    public string C_TextBin 
-	{
-		get => (string)GetValue(C_TextBinProperty);
-		set => SetValue(C_TextBinProperty, value);
-	}
 
-	public static BindableProperty C_TextBinProperty = BindableProperty.Create(nameof(C_TextBin), 
-		typeof(string), typeof(ComponentTest), default(string), BindingMode.OneWay);
+    public static BindableProperty ComponentTextProperty = BindableProperty.Create(nameof(ComponentText), typeof(string), typeof(ComponentTest), default(string), BindingMode.OneWay, propertyChanged: OnComponentTextChanged);
+
+    private static void OnComponentTextChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        if (bindable is ComponentTest componentTest)
+            componentTest.MyComponentLabel.Text = newValue.ToString();
+    }
+
+    public string ComponentText
+    {
+        get { return (string)GetValue(ComponentTextProperty); }
+        set { SetValue(ComponentTextProperty, value); }
+    }
+
+    public string C_TextBin
+    {
+        get => (string)GetValue(C_TextBinProperty);
+        set => SetValue(C_TextBinProperty, value);
+    }
+
+    public static BindableProperty C_TextBinProperty = BindableProperty.Create(nameof(C_TextBin),
+        typeof(string), typeof(ComponentTest), default(string), BindingMode.OneWay);
 
     public ComponentTest()
-	{
-		InitializeComponent();
-		BindingContext = this;
-	}
+    {
+        InitializeComponent();
+    }
 }
--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -1,28 +1,36 @@
-﻿namespace MauiApp1;
+﻿using MauiApp1.ViewModel;
+
+namespace MauiApp1;
 
 public partial class MainPage : ContentPage
 {
-	private string _textForBinding;
-    public string textForBinding 
-	{
-		get => _textForBinding;
-		set
-		{
-			_textForBinding = value;
-			OnPropertyChanged();
-		} 
-	}
+    //This property cannot be here because you set, on componentTest, itself as bindingcontext
+
+    //private string _textForBinding;
+    //   public string textForBinding 
+    //{
+    //	get => _textForBinding;
+    //	set
+    //	{
+    //		_textForBinding = value;
+    //		OnPropertyChanged();
+    //	} 
+    //}
 
     public MainPage()
-	{
-		InitializeComponent();
-		BindingContext = this;
-	}
+    {
+        InitializeComponent();
+        BindingContext = new MainPageViewModel();
+    }
 
     protected override void OnAppearing()
     {
         base.OnAppearing();
-        textForBinding = "test";
+
+        (this.BindingContext as MainPageViewModel).TextForBinding = "test";
+
+        //This is not the best way, only for study
+        //textForBinding = "test";
     }
 }
 
